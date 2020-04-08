4f9c3d14c25d7c3b5a3fa8609ee49da764a09bcc
lucasgoncalves <lucas.faiskaa@gmail.com>
2020-04-08 02:54:50 -0300

    Adding Splash Screen.

---
+++ app/src/main/java/com/lfaiska/bestmoviesseries/presentation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lfaiska.bestmoviesseries.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import com.lfaiska.bestmoviesseries.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
---
+++ app/src/main/java/com/lfaiska/bestmoviesseries/presentation/scenes/splash/SplashFragment.kt
@@ -0,0 +1,6 @@
+package com.lfaiska.bestmoviesseries.presentation.scenes.splash
+
+import androidx.fragment.app.Fragment
+import com.lfaiska.bestmoviesseries.R
+
+class SplashFragment : Fragment(R.layout.fragment_splash)
