772f99029173f2ccdccdca51af01edf3bd32f0c9
lucasgoncalves <lucas.faiskaa@gmail.com>
2020-04-10 01:04:57 -0300

    Setup Koin on Project.

--- app/src/main/java/com/lfaiska/bestmoviesseries/MainApplication.kt
+++ app/src/main/java/com/lfaiska/bestmoviesseries/MainApplication.kt
@@ -1,6 +1,19 @@
 package com.lfaiska.bestmoviesseries
 
 import android.app.Application
+import com.lfaiska.bestmoviesseries.data.remote.di.retrofitModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@MainApplication)
+            modules(listOf(retrofitModule))
+        }
+    }
 }
